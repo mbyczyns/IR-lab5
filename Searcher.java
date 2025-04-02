37fdc1d16aeae97037f77ae9ef56a66a072289e6
mbyczyns <mbyczyns@gmail.com>
2025-04-02 10:35:05 +0200

    update

--- Searcher.java
+++ Searcher.java
@@ -2,9 +2,7 @@ import org.apache.lucene.analysis.Analyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.document.IntPoint;
-import org.apache.lucene.index.DirectoryReader;
-import org.apache.lucene.index.IndexReader;
-import org.apache.lucene.index.Term;
+import org.apache.lucene.index.*;
 import org.apache.lucene.queryparser.classic.ParseException;
 import org.apache.lucene.queryparser.classic.QueryParser;
 import org.apache.lucene.search.*;
@@ -14,171 +12,89 @@ import org.apache.lucene.store.FSDirectory;
 import java.io.IOException;
 import java.nio.file.Paths;
 
-public class Searcher
-{
+public class Searcher {
 
-    public static void main(String args[])
-    {
-        // Load the previously generated index (DONE)
+    public static void main(String[] args) {
         IndexReader reader = getIndexReader();
         assert reader != null;
-
-        // Construct index searcher (DONE)
         IndexSearcher indexSearcher = new IndexSearcher(reader);
-        // Standard analyzer - might be helpful
         Analyzer analyzer = new StandardAnalyzer();
 
-        // TODO your task is to construct several queries and seek for relevant documents
+        try {
+            // 1) TERM QUERY: Searching for "mammal"
+            System.out.println("1) Term Query: mammal");
+            TermQuery tq1 = new TermQuery(new Term(Constants.content, analyzer.normalize(Constants.content, "mammal").utf8ToString()));
+            printResultsForQuery(indexSearcher, tq1);
 
-        // TERM QUERY
-        // A Query that matches documents containing a term.
-        // This may be combined with other terms with a BooleanQuery.
-        // TODO seek for documents that contain word mammal
-        // as you may notice, this word is not normalized (but is should be normalized
-        // in the same way as all documents were normalized when constructing the index.
-        // For that reason you can use analyzer object (utf8TOString()!).
-        // Then, build a Term object (seek in content - Constants.content) and TermQuery.
-        // Lastly, invoke printResultsForQuery.
-        String queryMammal = "MaMMal";
-        TermQuery tq1;
-        {
-            // --------------------------------------
-            // COMPLETE THE CODE HERE
-            System.out.println("1) term query: mammal (CONTENT)");
-         
-            // --------------------------------------
+            // 2) TERM QUERY: Searching for "bird"
+            System.out.println("2) Term Query: bird");
+            TermQuery tq2 = new TermQuery(new Term(Constants.content, analyzer.normalize(Constants.content, "bird").utf8ToString()));
+            printResultsForQuery(indexSearcher, tq2);
+
+            // 3) BOOLEAN QUERY: Searching for "mammal OR bird"
+            System.out.println("3) Boolean Query: mammal OR bird");
+            BooleanQuery booleanQuery = new BooleanQuery.Builder()
+                    .add(new BooleanClause(tq1, BooleanClause.Occur.SHOULD))
+                    .add(new BooleanClause(tq2, BooleanClause.Occur.SHOULD))
+                    .setMinimumNumberShouldMatch(1)
+                    .build();
+            printResultsForQuery(indexSearcher, booleanQuery);
+
+            // 4) RANGE QUERY: Searching for files smaller than 1000 bytes
+            System.out.println("4) Range Query: File size [0b, 1000b]");
+            Query rangeQuery = IntPoint.newRangeQuery(Constants.filesize, 0, 1000);
+            printResultsForQuery(indexSearcher, rangeQuery);
+
+            // 5) PREFIX QUERY: Searching for files starting with "ant"
+            System.out.println("5) Prefix Query: ant");
+            Query prefixQuery = new PrefixQuery(new Term(Constants.filename, "ant"));
+            printResultsForQuery(indexSearcher, prefixQuery);
+
+            // 6) WILDCARD QUERY: Searching for words matching "eat?"
+            System.out.println("6) Wildcard Query: eat?");
+            Query wildcardQuery = new WildcardQuery(new Term(Constants.content, "eat?"));
+            printResultsForQuery(indexSearcher, wildcardQuery);
+
+            // 7) FUZZY QUERY: Searching for "mamml"
+            System.out.println("7) Fuzzy Query: mamml");
+            Query fuzzyQuery = new FuzzyQuery(new Term(Constants.content, "mamml"));
+            printResultsForQuery(indexSearcher, fuzzyQuery);
+
+            // 8) QUERY PARSER: Parsing complex human queries
+            System.out.println("8) Query Parser: " + "MaMMal AND bat");
+            QueryParser parser = new QueryParser(Constants.content, analyzer);
+            Query parsedQuery = parser.parse("MaMMal AND bat");
+            printResultsForQuery(indexSearcher, parsedQuery);
+
+        } catch (IOException | ParseException e) {
+//            e.printStackTrace();
         }
 
-        // TODO Repeat the previous step for a word "bird".
-        // Compare the results for "mammal" and "bird".
-        String queryBird = "bird";
-        TermQuery tq2;
-        {
-            // --------------------------------------
-            System.out.println("2) term query bird (CONTENT)");
-        
-            // --------------------------------------
-        }
-
-        // TODO now, we seek for documents that contain "mammal" or "bird".
-        // Construct two clauses: BooleanClause (use BooleanClause.Occur to set a proper flag).
-        // The first concerns tq1 ("mammal") and the second concerns ("bird").
-        // To construct BooleanQuery, Use static methods of BooleanQuery.Builder().
-        // Additionally, use setMinimumNumberShouldMatch() with a proper parameter
-        // to generate "mammal" or "bird" rule.
-
-        // Boolean query
-        {
-            // --------------------------------------
-            System.out.println("3) boolean query (CONTENT): mammal or bird");
-       
-            // --------------------------------------
-        }
-
-        // TODO now, your task is to find all documents which size is smaller than 1000bytes.
-        // For this reason, construct Range query.
-        // Use IntPoint.newRangeQuery.
-        {
-            // --------------------------------------
-            System.out.println("4) range query: file size in [0b, 1000b]");
-          
-            // --------------------------------------
-        }
-
-        // TODO let's find all documents which name starts with "ant".
-        // For this reason, construct PrefixQuery.
-        {
-            // --------------------------------------
-            System.out.println("5) Prefix query (FILENAME): ant");
-         
-            // --------------------------------------
-        }
-
-        // TODO let's build a wildcard query".
-        // Construct a WildcardQuery object. Look for documents
-        // which contain a term "eat?" "?" stand for any letter (* for a sequence of letters).
-        {
-            // --------------------------------------
-            System.out.println("6) Wildcard query (CONTENT): eat?");
-    
-            // --------------------------------------
-        }
-
-        // TODO build a fuzzy query for a word "mamml" (use FuzzyQuerry).
-        // Find all documents that contain words which are similar to "mamml".
-        // Which documents have been found?
-        {
-            // --------------------------------------
-            System.out.println("7) Fuzzy querry (CONTENT): mamml?");
-     
-            // --------------------------------------
-        }
-
-        // TODO now, use QueryParser to parse human-entered query strings
-        // and generate query object.
-        // - use AND, OR , NOT, (, ), + (must), and - (must not) to construct boolean queries
-        // - use * and ? to contstruct wildcard queries
-        // - use ~ to construct fuzzy (one word, similarity) or proximity (at least two words) queries
-        // - use - to construct proximity queries
-        // - use \ as an escape character for: + - && || ! ( ) { } [ ] ^ " ~ * ? : \
-        // Consider following 5 examples of queries:
-        String queryP1 = "MaMMal AND bat";
-        String queryP2 = "ante*";
-        String queryP3 = "brd~ ";
-        String queryP4 = "(\"nocturnal life\"~10) OR bat";
-        String queryP5 = "(\"nocturnal life\"~10) OR (\"are nocturnal\"~10)";
-        // Select some query:
-        String selectedQuery = queryP1;
-        // Complete the code here, i.e., build query parser object, parse selected query
-        // to query object, and find relevant documents. Analyze the outcomes.
-        {
-            // --------------------------------------
-            System.out.println("8) query parser = " + selectedQuery);
-     
-            // --------------------------------------
-        }
-
-
-        try
-        {
+        try {
             reader.close();
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+        } catch (IOException e) {
+//            e.printStackTrace();
         }
     }
 
-    private static void printResultsForQuery(IndexSearcher indexSearcher, Query q)
-    {
-        // TODO finish this method
-        // - use indexSearcher to search for documents that
-        // are relevant according to the query q
-        // - Get TopDocs object (number of derived documents = Constant.top_docs)
-        // - Iterate over ScoreDocs (in in TopDocs) and print for each document (in separate lines):
-        // a) score
-        // b) filename
-        // c) id
-        // d) file size
-        // You may use indexSearcher to get a Document object for some docId (ScoreDoc)
-        // and use document.get(name of the field) to get the value of id, filename, etc.
+    private static void printResultsForQuery(IndexSearcher indexSearcher, Query q) throws IOException {
+        TopDocs topDocs = indexSearcher.search(q, Constants.top_docs);
+        System.out.println("Found " + topDocs.totalHits + " documents.");
 
-        // --------------------------------
-      
-        // --------------------------------
+        for (ScoreDoc scoreDoc : topDocs.scoreDocs) {
+            Document doc = indexSearcher.doc(scoreDoc.doc);
+            System.out.println("SCORE: " + scoreDoc.score + " | FILENAME: " + doc.get(Constants.filename) +
+                    " (ID=" + doc.get(Constants.id) + ") (Content=" + doc.get(Constants.content) + ") (Size=" + doc.get(Constants.filesize_int) + ")");
+        }
     }
 
-    private static IndexReader getIndexReader()
-    {
-        try
-        {
+    private static IndexReader getIndexReader() {
+        try {
             Directory dir = FSDirectory.open(Paths.get(Constants.index_dir));
             return DirectoryReader.open(dir);
-
-        } catch (IOException e)
-        {
-            e.printStackTrace();
+        } catch (IOException e) {
+//            e.printStackTrace();
         }
         return null;
     }
-
 }
